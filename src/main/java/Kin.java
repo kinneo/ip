273a079f33f9c442b5013c34bd2ce43ac9a3f573
kinneo <kinneo2002@gmail.com>
2025-01-27 22:25:39 +0800

    CS2113 Week 3 Echo

--- src/main/java/Kin.java
+++ src/main/java/Kin.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class Kin {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
         System.out.println("Hello! I'm Kin");
+        Scanner in = new Scanner(System.in);
         System.out.println("What can I do for you?");
-        System.out.println("Bye, hope to see you again soon!");
+        String line = "";
+        while (!(line.equals("bye"))) {
+            line = in.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                System.out.println("Bye, hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(line);
+            }
+        }
     }
 }
